72d83ecee5d3d791085891e4b94fefed40d47704
Petr Sloup <slouppetr@gmail.com>
2010-11-18 19:50:39 +0000

    SegmentedPlane improvements (mostly shader and UI)

--- we/scene/scene.js
+++ we/scene/scene.js
@@ -46,100 +46,97 @@ we.scene.Scene = function(context) {
 
   /**
    * @type {number}
+   * This should always equal Math.pow(2, this.zoomLevel) !
    */
-  this.distance = 0;
+  this.tileCount = 1;
 
   /**
    * @type {number}
    */
-  this.rotation = 0;
+  this.distance = -2;
 
   /**
    * @type {number}
    */
   this.latitude = 0;
 
+  /**
+   * @type {number}
+   */
+  this.longitude = 0;
+
   //alert(this.tileProvider.getTileURL(6, 50, 3));
 
+  var distanceSlider = new goog.ui.Slider;
+  distanceSlider.createDom();
+  var distanceSliderEl = distanceSlider.getElement();
+  distanceSliderEl.style.width = '640px';
+  distanceSliderEl.style.height = '10px';
+  distanceSlider.render(document.body);
+  distanceSlider.setStep(null);
+  distanceSlider.setMinimum(-8);
+  distanceSlider.setMaximum(0);
+
   var zoomSlider = new goog.ui.Slider;
-  zoomSlider.setOrientation(goog.ui.Slider.Orientation.HORIZONTAL);
   zoomSlider.createDom();
   var zoomSliderEl = zoomSlider.getElement();
-  zoomSliderEl.style.width = '200px';
+  zoomSliderEl.style.width = '640px';
   zoomSliderEl.style.height = '20px';
   zoomSlider.render(document.body);
-  zoomSlider.setStep(1);
+  zoomSlider.setStep(null);
   zoomSlider.setMinimum(2);
-  zoomSlider.setMaximum(20);
+  zoomSlider.setMaximum(20.99);
   var updateSceneZoomLevel = function(scene) {
     return (function() {
-      scene.zoomLevel = zoomSlider.getValue();
+      scene.zoomLevel = Math.min(Math.floor(zoomSlider.getValue()),
+          scene.tileProvider.getMaxZoomLevel());
+      scene.tileCount = Math.pow(2, scene.zoomLevel);
       document.getElementById('fpsbox').innerHTML = scene.zoomLevel;
+      scene.distance = -1 / Math.pow(2, zoomSlider.getValue() - 4.6);
+      distanceSlider.setValue(scene.distance);
     });
   };
   zoomSlider.addEventListener(goog.ui.Component.EventType.CHANGE,
       updateSceneZoomLevel(this));
   zoomSlider.setValue(4);
 
-  var distanceSlider = new goog.ui.Slider;
-  distanceSlider.setOrientation(goog.ui.Slider.Orientation.HORIZONTAL);
-  distanceSlider.createDom();
-  var distanceSliderEl = distanceSlider.getElement();
-  distanceSliderEl.style.width = '200px';
-  distanceSliderEl.style.height = '20px';
-  distanceSlider.render(document.body);
-  distanceSlider.setStep(null);
-  distanceSlider.setMinimum(-3);
-  distanceSlider.setMaximum(0.99999);
-  var updateSceneDistance = function(scene) {
-    return (function() {
-      scene.distance = distanceSlider.getValue();
-      document.getElementById('fpsbox').innerHTML = scene.distance;
-    });
-  };
-  distanceSlider.addEventListener(goog.ui.Component.EventType.CHANGE,
-      updateSceneDistance(this));
-  distanceSlider.setValue(-2);
-
-  var rotationSlider = new goog.ui.Slider;
-  rotationSlider.setOrientation(goog.ui.Slider.Orientation.HORIZONTAL);
-  rotationSlider.createDom();
-  var rotationSliderEl = rotationSlider.getElement();
-  rotationSliderEl.style.width = '200px';
-  rotationSliderEl.style.height = '20px';
-  rotationSlider.render(document.body);
-  rotationSlider.setStep(null);
-  rotationSlider.setMinimum(-Math.PI);
-  rotationSlider.setMaximum(Math.PI);
-  var updateSceneRotation = function(scene) {
-    return (function() {
-      scene.rotation = rotationSlider.getValue();
-      document.getElementById('fpsbox').innerHTML = scene.rotation;
-    });
-  };
-  rotationSlider.addEventListener(goog.ui.Component.EventType.CHANGE,
-      updateSceneRotation(this));
-  rotationSlider.setValue(0);
-
   var latitudeSlider = new goog.ui.Slider;
-  latitudeSlider.setOrientation(goog.ui.Slider.Orientation.HORIZONTAL);
   latitudeSlider.createDom();
   var latitudeSliderEl = latitudeSlider.getElement();
-  latitudeSliderEl.style.width = '200px';
+  latitudeSliderEl.style.width = '640px';
   latitudeSliderEl.style.height = '20px';
   latitudeSlider.render(document.body);
   latitudeSlider.setStep(null);
-  latitudeSlider.setMinimum(-Math.PI);
-  latitudeSlider.setMaximum(Math.PI);
+  latitudeSlider.setMinimum(-Math.PI / 2);
+  latitudeSlider.setMaximum(Math.PI / 2);
   var updateSceneLatitude = function(scene) {
     return (function() {
       scene.latitude = latitudeSlider.getValue();
-      document.getElementById('fpsbox').innerHTML = scene.latitude;
+      //document.getElementById('fpsbox').innerHTML = scene.latitude;
     });
   };
   latitudeSlider.addEventListener(goog.ui.Component.EventType.CHANGE,
       updateSceneLatitude(this));
   latitudeSlider.setValue(0);
+
+  var longitudeSlider = new goog.ui.Slider;
+  longitudeSlider.createDom();
+  var longitudeSliderEl = longitudeSlider.getElement();
+  longitudeSliderEl.style.width = '640px';
+  longitudeSliderEl.style.height = '20px';
+  longitudeSlider.render(document.body);
+  longitudeSlider.setStep(null);
+  longitudeSlider.setMinimum(-Math.PI / 2);
+  longitudeSlider.setMaximum(Math.PI / 2);
+  var updateSceneLongitude = function(scene) {
+    return (function() {
+      scene.longitude = longitudeSlider.getValue();
+      //document.getElementById('fpsbox').innerHTML = scene.longitude;
+    });
+  };
+  longitudeSlider.addEventListener(goog.ui.Component.EventType.CHANGE,
+      updateSceneLongitude(this));
+  longitudeSlider.setValue(0);
 
   var fsshader = we.gl.Shader.createFromElement(context, 'shader-fs');
   var vsshader = we.gl.Shader.createFromElement(context, 'shader-vs');
@@ -170,13 +167,16 @@ we.scene.Scene = function(context) {
 
   shaderProgram.zoomLevelUniform =
       gl.getUniformLocation(shaderProgram, 'uZoomLevel');
-  shaderProgram.latitudeUniform =
-      gl.getUniformLocation(shaderProgram, 'uLatitude');
+  shaderProgram.tileCountUniform =
+      gl.getUniformLocation(shaderProgram, 'uTileCount');
+  shaderProgram.yOffsetUniform =
+      gl.getUniformLocation(shaderProgram, 'uYOffset');
+  shaderProgram.xOffsetUniform =
+      gl.getUniformLocation(shaderProgram, 'uXOffset');
 
   we.program = shaderProgram;
 
   we.plane = new we.scene.SegmentedPlane(context, 32, 32, 2);
-  //new we.gl.Plane(context, 5, 2.5);
   we.texture = we.gl.Texture.load(context,
       'http://a.tile.openstreetmap.org/0/0/0.png');
 };
@@ -188,8 +188,12 @@ we.scene.Scene = function(context) {
 we.scene.Scene.prototype.draw = function() {
   var gl = this.context.gl;
 
-  this.context.translate(0, 0, -2 + this.distance);
-  this.context.rotate(this.rotation, 1, 0, 0);
+  this.context.translate(0, 0, -1 + this.distance
+      /*/(Math.sin(Math.abs(this.latitude))+1.0)*/);
+  this.context.rotate(this.latitude, 1, 0, 0);
+  this.context.rotate(
+      -(this.longitude / (Math.PI) * this.tileCount % 1.0) / this.tileCount *
+      (2 * Math.PI), 0, 1, 0);
 
   gl.activeTexture(gl.TEXTURE0);
   gl.bindTexture(gl.TEXTURE_2D, we.texture);
@@ -201,22 +205,26 @@ we.scene.Scene.prototype.draw = function() {
 
   gl.bindBuffer(gl.ARRAY_BUFFER, we.plane.vertexBuffer);
   gl.vertexAttribPointer(we.program.vertexPositionAttribute,
-                         we.plane.vertexBuffer.itemSize,
-                         gl.FLOAT, false, 0, 0);
+      we.plane.vertexBuffer.itemSize,
+      gl.FLOAT, false, 0, 0);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, we.plane.texCoordBuffer);
   gl.vertexAttribPointer(we.program.textureCoordAttribute,
       we.plane.texCoordBuffer.itemSize,
       gl.FLOAT, false, 0, 0);
 
-  //gl.uniformMatrix4fv(we.program.pMatrixUniform, false, projection);
   gl.uniformMatrix4fv(we.program.mvpMatrixUniform, false, mvpm);
   gl.uniform1f(we.program.zoomLevelUniform, this.zoomLevel);
-  gl.uniform1f(we.program.latitudeUniform, this.latitude);
+  gl.uniform1f(we.program.tileCountUniform, this.tileCount);
+  var yOffset = Math.floor(Math.log(
+      Math.tan(this.latitude / 2.0 + Math.PI / 4.0)
+      ) / (Math.PI * 2) * this.tileCount);
+  gl.uniform1f(we.program.yOffsetUniform, yOffset);
+  gl.uniform1f(we.program.xOffsetUniform,
+      Math.floor(this.longitude / (Math.PI) * this.tileCount));
 
   gl.drawArrays(gl.TRIANGLES, 0, we.plane.vertexBuffer.numItems);
   //gl.drawArrays(gl.LINES, 0, we.plane.vertexBuffer.numItems);
-
 };
 
 if (goog.DEBUG) {
