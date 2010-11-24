ef75b328dd59bdcbc71f15794938716b1b46239b
Petr Sloup <slouppetr@gmail.com>
2010-11-24 22:41:01 +0000

    Vertex shader optimizations
    GPU ShaderAnalyzer now reports 85-87 ALU instructions instead of 96-98.

--- vs.glsl
+++ vs.glsl
@@ -2,11 +2,12 @@
 #define BUFFER_HEIGHT %BUFFER_HEIGHT_FLOAT%
 #define BUFFER_SIZE %BUFFER_SIZE_INT%
 
+#ifdef GL_ES
 precision highp float;
+#endif
 
 const float PI  = 3.1415927;
 const float PI2 = 6.2831855;
-const float PIhalf = 1.5707963;
 const float MAX_PHI = 1.4844222;
 
 attribute vec2 aVertexPosition;
@@ -23,63 +24,65 @@ uniform sampler2D uTileBuffer;
 uniform vec4 uMetaBuffer[BUFFER_SIZE];
 varying vec2 vTC;
 
-float compareMeta(vec4 a, float zoom, float x, float y) {
-  return ((a.r == zoom) ? ((a.g == x) ? ((a.b == y) ? 0.0 : (a.b - y)) : (a.g - x)) : (a.r - zoom));
+float compareMeta(in vec3 a, in vec3 b) {
+  vec3 c = a - b;
+  return bool(c.x) ? c.x : (bool(c.y) ? c.y : c.z);
 }
 
 void main(void) {
-  float phix = (aVertexPosition.x)/uTileCount*PI2;
-  float phiy = (uOffset.y+aVertexPosition.y)/uTileCount*PI2;
+  vec2 phi = PI2*vec2(aVertexPosition.x, aVertexPosition.y + uOffset.y)/uTileCount;
   
   //this version has better accuracy than 2.0*atan(exp(uLatitude + phiy)) - PI/2.0
   // (is more numerically stable)
-  phiy = atan((exp(phiy) - exp(-phiy))/2.0);
+  phi.y = atan((exp(phi.y) - exp(-phi.y))*0.5);
   
   //if (abs(phiy) > MAX_PHI)
   //  phiy = sign(phiy)*PIhalf;
     
-  if (abs(phix) > PI)
-    phix = PI;        
-  if (abs(phiy) > PI)
-    phiy = PI;
+  if (abs(phi.x) > PI)
+    phi.x = PI;
+  if (abs(phi.y) > PI)
+    phi.y = PI;
+
+  gl_Position = uMVPMatrix * vec4(sin(phi.x)*cos(phi.y), sin(phi.y), cos(phi.x)*cos(phi.y), 1.0);      
   
-  gl_Position = uMVPMatrix * vec4(sin(phix)*cos(phiy), sin(phiy), cos(abs(phix))*cos(abs(phiy)), 1.0);      
+  float tilex = mod((aVertexPosition.x - aTextureCoord.x  + uOffset.x + uTileCount*0.5), uTileCount);
+  float tiley = aTextureCoord.y - 1.0 - aVertexPosition.y - uOffset.y + uTileCount*0.5;
+  vec2 off = vec2(0.0, 0.0);
   
-  float tilex = mod((aVertexPosition.x - aTextureCoord.x  + uOffset.x + uTileCount/2.0), uTileCount);
-  float tiley = (uTileCount-1.0) - (aVertexPosition.y - aTextureCoord.y + uOffset.y + uTileCount/2.0);
-  float xoff = 0.0, yoff = 0.0;
+  vec3 key = vec3(uZoomLevel, tilex, tiley);
   
-  int mid = 1, min = 1, max = BUFFER_SIZE;
-  float z = uZoomLevel;
-  while (z >= 0.0 && (compareMeta(uMetaBuffer[mid-1],z,tilex,tiley) != 0.0)) {
+  const float last = float(BUFFER_SIZE)-1.0;
+  int mid = 1; float min = 0.0, max = last;
+  while (key.r >= 0.0 && (compareMeta(uMetaBuffer[mid].xyz,key) != 0.0)) {
       if (min > max) {
-        z--;
-        min = 1;
-        max = BUFFER_SIZE;
-        xoff = xoff/2.0 + mod(tilex, 2.0);
-        yoff = yoff/2.0 + 1.0-mod(tiley, 2.0);
-        tilex = floor(tilex/2.0);
-        tiley = floor(tiley/2.0);
+        key.r--;
+        min = 0.0;
+        max = last;
+        off.x = off.x*0.5 + mod(key.g, 2.0);
+        off.y = off.y*0.5 + 1.0-mod(key.b, 2.0);
+        key.gb = floor(key.gb/2.0);
+        //key.b = floor(key.b/2.0);
       }
       
-      mid = (min + max) / 2;
-      if (compareMeta(uMetaBuffer[mid-1],z,tilex,tiley) > 0.0) {
-        max = mid - 1;
+      mid = int((min + max) * 0.5);
+      if (compareMeta(uMetaBuffer[mid].xyz,key) > 0.0) {
+        max = float(mid) - 1.0;
       } else {
-        min = mid + 1;
+        min = float(mid) + 1.0;
       }
   }
   
-  if (compareMeta(uMetaBuffer[mid-1],z,tilex,tiley) == 0.0) {
-    float i = uMetaBuffer[mid-1].a;
-    float reduction = pow(2.0,uZoomLevel - z);
-    vTC.x = ((mod(i, BUFFER_WIDTH)) + xoff/2.0  + (aTextureCoord.x)/reduction)/BUFFER_WIDTH;
-    vTC.y = ((floor(i / BUFFER_WIDTH)) + yoff/2.0 + (aTextureCoord.y)/reduction)/BUFFER_HEIGHT;
+  if (compareMeta(uMetaBuffer[mid].xyz,key) == 0.0) {
+    float i = uMetaBuffer[mid].a;
+    float reduction = exp2(uZoomLevel - key.r);
+    vTC.x = ((mod(i, BUFFER_WIDTH)) + off.x*0.5  + (aTextureCoord.x)/reduction)/BUFFER_WIDTH;
+    vTC.y = ((floor(i / BUFFER_WIDTH)) + off.y*0.5 + (aTextureCoord.y)/reduction)/BUFFER_HEIGHT;
     return;
   }
   
   vTC = vec2(0.0,0.0);//aTextureCoord;
   
-  if ((abs(phiy) - MAX_PHI) > 0.01)
+  if ((abs(phi.y) - MAX_PHI) > 0.01)
     vTC=vec2(0.5,0.5); //DEBUG
 }
