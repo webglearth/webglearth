19ecdf55c4919e4c55aa1734735644fe8a22a673
Petr Sloup <slouppetr@gmail.com>
2011-01-17 13:25:11 +0000

    MouseZoomer improvements - DOUBLECLICK now zooms 1 level in and RIGHTCLICK 1 level out

--- we/ui/mousezoomer.js
+++ we/ui/mousezoomer.js
@@ -40,6 +40,38 @@ we.ui.MouseZoomer = function(scene) {
         this.setZoom(newLevel);
         e.preventDefault();
       }, scene));
+
+  /**
+   * @type {?number}
+   * @private
+   */
+  this.dblClickListenKey_ = goog.events.listen(scene.context.canvas,
+      goog.events.EventType.DBLCLICK,
+      goog.bind(function(e) {
+        this.setZoom(this.zoomLevel + 1);
+        e.preventDefault();
+      }, scene));
+
+  /**
+   * @type {?number}
+   * @private
+   */
+  this.rightClickListenKey_ = goog.events.listen(scene.context.canvas,
+      goog.events.EventType.MOUSEUP,
+      goog.bind(function(e) {
+        if (e.isButton(goog.events.BrowserEvent.MouseButton.RIGHT)) {
+          e.preventDefault();
+          this.setZoom(this.zoomLevel - 1);
+        }
+      }, scene));
+
+  /**
+   * preventDefault on MOUSEUP is just not enough
+   * @type {?number}
+   * @private
+   */
+  this.noContextMenuListenKey_ = goog.events.listen(scene.context.canvas,
+      goog.events.EventType.CONTEXTMENU, function(e) {e.preventDefault();});
 };
 goog.inherits(we.ui.MouseZoomer, goog.Disposable);
 
@@ -48,5 +80,8 @@ goog.inherits(we.ui.MouseZoomer, goog.Disposable);
 we.ui.MouseZoomer.prototype.disposeInternal = function() {
   //goog.base(this, 'disposeInternal');
   goog.events.unlistenByKey(this.listenKey_);
+  goog.events.unlistenByKey(this.dblClickListenKey_);
+  goog.events.unlistenByKey(this.rightClickListenKey_);
+  goog.events.unlistenByKey(this.noContextMenuListenKey_);
   this.mouseWheelHandler_.dispose();
 };
