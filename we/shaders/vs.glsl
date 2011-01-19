5911d98c8ab56826a732139eab71168a69964410
Petr Sloup <slouppetr@gmail.com>
2011-01-19 12:52:22 +0000

    Shader licence change

--- we/shaders/vs.glsl
+++ we/shaders/vs.glsl
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2011 Klokan Technologies GmbH (info@klokantech.com)
  *
- * The JavaScript code in this page is free software: you can
+ * The code in this file is free software: you can
  * redistribute it and/or modify it under the terms of the GNU
  * General Public License (GNU GPL) as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option)
