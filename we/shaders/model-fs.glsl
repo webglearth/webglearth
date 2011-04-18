354f349c7d09fa7be2c7ee7a398ab846fa4fca84
Tom Payne <tom.payne@camptocamp.com>
2011-04-18 15:36:22 +0200

    Make buildings gray

--- we/shaders/model-fs.glsl
+++ we/shaders/model-fs.glsl
@@ -3,6 +3,6 @@ precision highp float;
 varying vec3 vLightWeighting;
 
 void main(void) {
-  vec3 uColor = vec3(1.0, 0.0, 1.0);
+  vec3 uColor = vec3(0.8, 0.8, 0.8);
   gl_FragColor = vec4(uColor * vLightWeighting, 1.0);
 }
--- we/shaders/model-vs.glsl
+++ we/shaders/model-vs.glsl
@@ -12,8 +12,8 @@ void main(void) {
   gl_Position = uMVPMatrix * vec4(aVertexPosition, 1.0);
   vec3 transformedNormal = uNMatrix * aVertexNormal;
   vec3 uLightingDirection = vec3(1.0, 1.0, -1.0);
-  vec3 uAmbientColor = vec3(0.1, 0.1, 0.1);
-  vec3 uDirectionalColor = vec3(0.9, 0.9, 0.9);
+  vec3 uAmbientColor = vec3(0.2, 0.2, 0.2);
+  vec3 uDirectionalColor = vec3(0.8, 0.8, 0.8);
   float directionalLightWeighting = max(dot(transformedNormal, uLightingDirection), 0.0);
   vLightWeighting = uAmbientColor + uDirectionalColor * directionalLightWeighting;
 }
