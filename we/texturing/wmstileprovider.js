5c2453c6e1d2fd0058ce51fe5d85843392f0f032
Petr Sloup <slouppetr@gmail.com>
2011-08-25 09:02:37 +0200

    Modify the BoundingBox calculation in WMSTileProvider to assume the Earth to be perfect sphere - works better with Spherical Mercator

--- we/texturing/wmstileprovider.js
+++ we/texturing/wmstileprovider.js
@@ -179,7 +179,7 @@ we.texturing.WMSTileProvider.prototype.getTileSize = function() {
 
 /** @inheritDoc */
 we.texturing.WMSTileProvider.prototype.getTileURL = function(zoom, x, y) {
-
+  //TODO: Clean, optimize and reuse already implemented functions
 
   function tile2lon(x, z) {
     return (x / Math.pow(2, z) * 360 - 180);
@@ -195,8 +195,7 @@ we.texturing.WMSTileProvider.prototype.getTileURL = function(zoom, x, y) {
   }
 
   function merc_x(lon) {
-    var r_major = 6378137.000;
-    return r_major * deg_rad(lon);
+    return we.scene.EARTH_RADIUS * deg_rad(lon);
   }
 
   function merc_y(lat) {
@@ -204,18 +203,9 @@ we.texturing.WMSTileProvider.prototype.getTileURL = function(zoom, x, y) {
       lat = 89.5;
     if (lat < -89.5)
       lat = -89.5;
-    var r_major = 6378137.000;
-    var r_minor = 6356752.3142;
-    var temp = r_minor / r_major;
-    var es = 1.0 - (temp * temp);
-    var eccent = Math.sqrt(es);
     var phi = deg_rad(lat);
-    var sinphi = Math.sin(phi);
-    var con = eccent * sinphi;
-    var com = .5 * eccent;
-    con = Math.pow((1.0 - con) / (1.0 + con), com);
-    var ts = Math.tan(.5 * (Math.PI * 0.5 - phi)) / con;
-    var y = 0 - r_major * Math.log(ts);
+    var ts = Math.tan(.5 * (Math.PI * 0.5 - phi));
+    var y = 0 - we.scene.EARTH_RADIUS * Math.log(ts);
     return y;
   }
 
