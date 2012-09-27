fe468dc891fffb5705d34f249c980320965c0dff
Petr Sloup <slouppetr@gmail.com>
2012-09-27 14:50:11 +0200

    api/: Example usage of the miniglobe.

--- api/exports/exports.js
+++ api/exports/exports.js
@@ -156,8 +156,19 @@ goog.exportSymbol('WebGLEarth.prototype.handleResize', function() {
 
 goog.exportSymbol('WebGLEarth.prototype.saveScreenshot', function(name) {
   this.context.afterFrameOnce = goog.bind(function() {
-    we.canvas2image.saveCanvasAsPNG(this.context.canvas, name);
+    we.canvas2image.saveCanvasAsPNG(this.context.canvas, name,
+                                    this.markerManager);
   }, this);
+});
+
+goog.exportSymbol('WebGLEarth.prototype.showMiniGlobe', function(src, size) {
+  if (goog.isDefAndNotNull(src)) {
+    this.context.scene.miniGlobe =
+        new we.scene.MiniGlobe(this.context.scene, 32, 32, src);
+    this.context.scene.miniGlobe.setSize(size);
+  } else {
+    this.context.scene.miniGlobe = null;
+  }
 });
 
 goog.exportSymbol('WebGLEarth.Maps', weapi.maps.MapType);
